39ac02835ab8f620504adf8bae706bdd128e3373
tgrana2g <tgrana2g@gmail.com>
2023-08-29 10:08:10 -0600

    Index.php page, some customizations, css, images

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>El Furgoncito</title>
+    <link rel="icon" href="../img/hamburger.ico">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/index.css">
+</head>
+<body>
+
+<?php include 'navbar.php' ?> <!-- NAVBAR -->
+
+<div class="gif-container">
+    <h1>El Furgoncito</h1>
+    <div class="image-container">
+        
+        <img src="../img/ketchup-burger.gif" alt="">
+
+    </div> 
+</div>
+    
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="">MENU <span class="burger-icon"><img src="../img/hamburger.ico" alt=""></span></a></li>
+        <li><a href="">CARRITO <span class="burger-icon"><img src="../img/hamburger.ico" alt=""></span></a></li>
+        <li><a href="">NOSOTROS <span class="burger-icon"><img src="../img/hamburger.ico" alt=""></span></a></li>
+    </ul>
+</nav>
